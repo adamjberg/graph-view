2e07fd059e920b7f0edb195d999f43adb9737b95
Adam Berg <adam@xyzdigital.com>
2021-10-16 18:19:54 -0700

    Start to adam.xyzdigital.com constellation

--- Node.ts
+++ Node.ts
@@ -1,18 +1,42 @@
-import { Container, Graphics, Text } from 'pixi.js'
+import { Container, Graphics, Sprite, Text, TextStyle, Texture } from "pixi.js";
 
 export class Node extends Container {
-  constructor(msg: string) {
+  constructor(params: { title: string; url?: string; x: number; y: number, radius?: number, fontSize?: number }) {
     super();
 
+    this.x = params.x;
+    this.y = params.y;
+
+    this.interactive = true;
+    this.cursor = "hand";
+
+    this.on("mousedown", (e) => {
+      e.stopPropagation();
+    });
+
+    if (params.url) {
+      this.on("click", (e) => {
+        window.open(params.url, "_blank");
+      });
+    }
+
+    const circleRadius = params.radius || 10;
+
     const graphics = new Graphics();
-    graphics.beginFill(0xFF0000);
-    graphics.drawCircle(0, 0, 20)
+    graphics.beginFill(0xffffff);
+    graphics.drawCircle(0, 0, circleRadius);
     graphics.endFill();
 
-    const text = new Text(msg);
-    text.x = -text.width / 2;
-    text.y = 20;
-    this.addChild(text)
+    const style = new TextStyle({
+      fill: ["white", "white"],
+      fontSize: params.fontSize || 12,
+      align: "center"
+    });
+    const text = new Text(params.title, style);
+    text.anchor.set(0.5);
+    // text.x = -text.width / 2;
+    text.y = -text.height - circleRadius;
+    this.addChild(text);
 
     this.addChild(graphics);
   }
--- app.ts
+++ app.ts
@@ -1,38 +1,142 @@
-import { Application, Graphics } from "pixi.js";
+import { Application, Graphics, Sprite, Texture } from "pixi.js";
+import { Viewport } from "pixi-viewport";
 import { Node } from "./Node";
 
 const app = new Application({
   view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
   resolution: window.devicePixelRatio || 1,
   autoDensity: true,
-  backgroundColor: 0x6495ed,
+  backgroundColor: 0x0,
+  resizeTo: window,
 });
 
-const n1 = new Node("Hello");
-const n2 = new Node("World");
+const viewport = new Viewport({
+  screenWidth: window.innerWidth,
+  screenHeight: window.innerHeight,
+  worldWidth: 1000,
+  worldHeight: 1000,
+  interaction: app.renderer.plugins.interaction, // the interaction module is important for wheel to work properly when renderer.view is placed or scaled
+});
 
-const links = [[n1, n2]];
+viewport.moveCenter(0, 0);
 
-n1.x = app.view.width / 2;
-n1.y = app.view.height / 2;
-n2.x = app.view.width / 2 + 100;
-n2.y = app.view.height / 2;
+app.stage.addChild(viewport);
 
-app.stage.addChild(n1);
-app.stage.addChild(n2);
+// activate plugins
+viewport.drag().decelerate();
 
 const linkGraphics = new Graphics();
-for (const link of links) {
+viewport.addChild(linkGraphics);
+
+const nodes = [
+  {
+    _id: "1",
+    title: "adam.xyzdigital.com",
+    url: "https://adam.xyzdigital.com",
+    x: 0,
+    y: 0,
+    fontSize: 24
+  },
+  {
+    _id: "2",
+    title: "Projects",
+    x: 0,
+    y: 100,
+    fontSize: 18
+  },
+  {
+    _id: "3",
+    title: "Kaizen Music",
+    url: "https://kaizen.place",
+    x: 0,
+    y: 200,
+  },
+  {
+    _id: "4",
+    title: "engram",
+    url: "https://engramhq.xyz",
+    x: 150,
+    y: 200,
+  },
+  {
+    _id: "5",
+    title: "Learn",
+    url: "https://learn.xyzdigital.com",
+    x: -150,
+    y: 200,
+  },
+  {
+    _id: "6",
+    title: "Personal",
+    x: -150,
+    y: -100,
+    fontSize: 18
+  },
+  {
+    _id: "7",
+    title: "Random Thoughts After\n29 Laps Around the Sun",
+    url: "https://adam.xyzdigital.com/personal/random-thoughts-after-29-laps-around-the-sun",
+    x: -150,
+    y: -200
+  },
+  {
+    _id: "8",
+    title: "What Computer Architecture\nCan Teach Us About Anxiety",
+    url: "https://adam.xyzdigital.com/tech/software/what-computer-architecture-can-teach-us-about-anxiety",
+    x: 0,
+    y: -200
+  },
+  {
+    _id: "9",
+    title: "Mental Health",
+    x: 0,
+    y: -100,
+    fontSize: 18
+  },
+  {
+    _id: "10",
+    title: "Software",
+    x: 150,
+    y: -100,
+    fontSize: 18
+  },
+];
+
+const nodeLinkDataArray = [
+  ["1", "2"],
+  ["1", "6"],
+  ["1", "9"],
+  ["1", "10"],
+  ["2", "3"],
+  ["2", "4"],
+  ["2", "5"],
+  ["6", "7"],
+  ["8", "9"],
+  ["8", "10"]
+];
+
+const nodesById = {};
+
+for (const nodeData of nodes) {
+  const node = new Node(nodeData);
+  viewport.addChild(node);
+  nodesById[nodeData._id] = node;
+}
+
+const nodeLinks = [];
+
+for (const nodeLinkData of nodeLinkDataArray) {
+  nodeLinks.push([nodesById[nodeLinkData[0]], nodesById[nodeLinkData[1]]]);
+}
+
+for (const link of nodeLinks) {
   const [node1, node2] = link;
 
   linkGraphics.lineStyle({
-    color: 0x00FF00,
-    width: 2
-  })
-  linkGraphics.beginFill(0x00FF00)
-  linkGraphics.moveTo(node1.x, node1.y)
-  linkGraphics.lineTo(node2.x, node2.y)
-  linkGraphics.endFill()
+    color: 0x333333,
+    width: 2,
+  });
+  linkGraphics.moveTo(node1.x, node1.y);
+  linkGraphics.lineTo(node2.x, node2.y);
+  linkGraphics.endFill();
 }
-
-app.stage.addChild(linkGraphics);
